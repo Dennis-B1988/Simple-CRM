ec79839e028771b4067f4ef01c96c5bd2a462875
Dennis.B <Baust.Dennis@gmail.com>
2024-07-08 14:50:54 +0200

    $

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
