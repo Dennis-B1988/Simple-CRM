40f2a66e7fd52dc1e4717f8bf93113d4a8f32fca
Dennis.B <Baust.Dennis@gmail.com>
2024-07-08 16:24:18 +0200

    $

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule, TooltipPosition } from '@angular/material/tooltip';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+  above: TooltipPosition = 'above';
+  readonly dialog = inject(MatDialog);
+
+
+  openDialog(): void {
+    this.dialog.open(DialogAddUserComponent);
+  }
 
 }
