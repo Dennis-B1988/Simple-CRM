843e734fe764dfb13c3bc5e11787714879938be0
Dennis.B <Baust.Dennis@gmail.com>
2024-07-26 02:46:25 +0200

    get data from database

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,32 +1,43 @@
-import { Component, inject } from '@angular/core';
-import { Firestore, collection } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule, TooltipPosition } from '@angular/material/tooltip';
+import { Observable } from 'rxjs';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../models/user.class';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatCardModule],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatCardModule, CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
 
   above: TooltipPosition = 'above';
   readonly dialog = inject(MatDialog);
 
-  user = new User();
-  firestore: Firestore = inject(Firestore);
-  // users: User[] = [];
-  usersCollection = collection(this.firestore, 'users');
+  users$: Observable<User[]>;
+  allUsers: User[] = [];
 
 
-  constructor() {}
+  constructor(private firestore: Firestore) {
+    const usersCollection = collection(this.firestore, 'users');
+    this.users$ = collectionData(usersCollection, { idField: 'id' }) as Observable<User[]>;
+  }
+
+
+  ngOnInit(): void{
+    this.users$.subscribe((changes: any) => {
+      console.log(changes);
+      this.allUsers = changes;
+    });
+  }
 
 
   openDialog(): void {
